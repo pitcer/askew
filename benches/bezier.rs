a056e788966323dbadd84354bf54357b44fee899
Piotr Dobiech <pitcer@protonmail.com>
2023-05-31 13:47:19 +0200

    Add new rational bezier curve algorithm benchmark

--- benches/bezier.rs
+++ benches/bezier.rs
@@ -1,6 +1,7 @@
 use criterion::{criterion_group, criterion_main, Criterion};
 use rand::Rng;
 
+use askew::canvas::curve::control_points::rational_bezier::RationalBezierPoint;
 use askew::canvas::math;
 use askew::canvas::math::point::Point;
 
@@ -18,6 +19,15 @@ fn bench_bezier(criterion: &mut Criterion) {
     group.bench_function("chudy_wozny", |bencher| {
         bencher.iter(|| math::chudy_wozny(&points, t))
     });
+
+    let rational_points = points
+        .iter()
+        .map(|point| RationalBezierPoint::new(*point, 1.0))
+        .collect::<Vec<_>>();
+    group.bench_function("rational_chudy_wozny_same_coefficients", |bencher| {
+        bencher.iter(|| math::rational_chudy_wozny(&rational_points, t))
+    });
+
     group.finish();
 }
 
