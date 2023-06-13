9c98be0938059c5cbbb768287bb50b7c09b2d39b
Piotr Dobiech <pitcer@protonmail.com>
2023-06-13 04:54:02 +0200

    Simplify rgb parser
    For removed then_ignore(end()) see chumsky issue 298

--- src/config/rgb_parser.rs
+++ src/config/rgb_parser.rs
@@ -14,13 +14,11 @@ pub fn parse_rgb(input: &str) -> Result<Rgb> {
 }
 
 fn parser<'a>() -> impl Parser<'a, &'a [u8], Rgb> {
-    let color_byte = any()
-        .filter(u8::is_ascii_hexdigit)
-        .repeated()
-        .exactly(2)
-        .map_slice(|digits| u8::from_radix_16(digits).0);
+    let hex_digit = any().filter(u8::is_ascii_hexdigit);
+    let color_byte = group([hex_digit, hex_digit])
+        .map(|digits| u8::from_radix_16(&digits))
+        .map(|(color_byte, _)| color_byte);
     just(b'#')
         .ignore_then(group((color_byte, color_byte, color_byte)))
-        .then_ignore(end())
         .map(|(red, green, blue)| Rgb::new(red, green, blue))
 }
